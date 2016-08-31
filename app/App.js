67cee19d9011f3ea381618257ac8a66065704b22
huoguanghua <huoguanghua@infomedia.com.cn>
2016-08-31 23:20:59 +0800

    commit

---
+++ app/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+// Parent Component
+class GroceryList extends Component {
+    render() {
+        return (
+            <ul>
+                <ListItem quantity="1" name="Bread" />
+                <ListItem quantity="6" name="Eggs" />
+                <ListItem quantity="2" name="Milk" />
+            </ul>
+        );
+    }
+}
+
+// Child Component 
+class ListItem extends Component {
+    render() {
+        return (
+            <li>
+                {this.props.quantity}•{this.props.name}
+            </li>
+        );
+    }
+}
+
+render(<GroceryList />, document.getElementById('root'));
